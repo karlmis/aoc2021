185ed7f236d97a613d70f0ce30d8e2863e0eaa1d
karlmis <karlmissiaen@gmail.com>
2021-12-02 07:52:27 +0100

    day 2 some cleaning up

--- src/main/java/be/durvenproeven/aoc2021/day2/Sub.java
+++ src/main/java/be/durvenproeven/aoc2021/day2/Sub.java
@@ -1,44 +1,48 @@
 package be.durvenproeven.aoc2021.day2;
 
 public class Sub {
-	private int depth=0;
-	private int xPosition=0;
-	private int aim =0;
+	private int depth = 0;
+	private int xPosition = 0;
+	private int aim = 0;
 
 	public Sub() {
 	}
 
-	public void move(String input){
+	public void move(String input) {
 		String[] s = input.split(" ");
-		if (s[0].equals("forward")){
-			xPosition+= Integer.parseInt(s[1]);
+		String direction = s[0];
+		int nr = Integer.parseInt(s[1]);
+
+		if (direction.equals("forward")) {
+			this.xPosition += nr;
 			return;
 		}
-		if (s[0].equals("down")){
-			depth+= Integer.parseInt(s[1]);
+		if (direction.equals("down")) {
+			depth += nr;
 			return;
 		}
-		if (s[0].equals("up")){
-			depth-= Integer.parseInt(s[1]);
+		if (direction.equals("up")) {
+			depth -= nr;
 			return;
 		}
 		throw new UnsupportedOperationException();
 	}
 
-	public void moveWithAim(String input){
+	public void moveWithAim(String input) {
 		String[] s = input.split(" ");
-		if (s[0].equals("forward")){
-			int nr = Integer.parseInt(s[1]);
+		String direction = s[0];
+		int nr = Integer.parseInt(s[1]);
+		if (direction.equals("forward")) {
 			this.xPosition += nr;
-			depth+= nr *aim;
+			depth += nr * aim;
 			return;
 		}
-		if (s[0].equals("down")){
-			aim += Integer.parseInt(s[1]);
+		if (direction.equals("down")) {
+			aim += nr;
 			return;
 		}
-		if (s[0].equals("up")){
-			aim -= Integer.parseInt(s[1]);
+		if (direction.equals("up")) {
+			aim -= nr;
 			return;
 		}
 		throw new UnsupportedOperationException();

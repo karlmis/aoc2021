e8026d6dab69dccb3cd83b2c95757cc88c4fe4d4
karlmis <karlmissiaen@gmail.com>
2021-12-03 15:51:23 +0100

    day 3 small clean up

--- src/main/java/be/durvenproeven/aoc2021/day3/BinaryDiagnostic.java
+++ src/main/java/be/durvenproeven/aoc2021/day3/BinaryDiagnostic.java
@@ -1,7 +1,6 @@
 package be.durvenproeven.aoc2021.day3;
 
 import java.math.BigInteger;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -10,7 +9,7 @@ import static java.util.stream.Collectors.groupingBy;
 
 public class BinaryDiagnostic {
 	private final int size;
-	private int gammaRate;
+	private final int gammaRate;
 	private final List<Integer> nrs;
 
 	public BinaryDiagnostic(List<String> binaries, int size) {
@@ -18,15 +17,21 @@ public class BinaryDiagnostic {
 		nrs = binaries.stream()
 				.map(s -> Integer.parseInt(s, 2))
 				.toList();
+		gammaRate= calculateGammaRate(binaries, size);
+	}
+
+	private int calculateGammaRate(List<String> binaries, int size) {
+		int nrToBuildUp=0;
 		for (int i = 0; i < size; i++) {
-			int powerOf2 = i;
+			int bitIndex = i;
 			long count = nrs.stream()
-					.filter(nr -> BigInteger.valueOf(nr).testBit(powerOf2))
+					.filter(nr -> BigInteger.valueOf(nr).testBit(bitIndex))
 					.count();
 			if (count > binaries.size() / 2) {
-				gammaRate += powerOf2(powerOf2);
+				nrToBuildUp += powerOf2(bitIndex);
 			}
 		}
+		return nrToBuildUp;
 	}
 
 	private int getSize(List<Integer> integers) {
@@ -43,19 +48,19 @@ public class BinaryDiagnostic {
 
 	public int getCo2ScrubberRating() {
 		Function<Map<Boolean, List<Integer>>, List<Integer>> selector = l -> selectLeastCommon(l, false);
-		return reduceListTillOne(selector, size - 1, new ArrayList<Integer>(nrs));
+		return reduceListTillOne(selector, size - 1, nrs);
 	}
 
-	private int reduceListTillOne(Function<Map<Boolean, List<Integer>>, List<Integer>> selector, int downwardCounter, List<Integer> nrs1) {
+	private int reduceListTillOne(Function<Map<Boolean, List<Integer>>, List<Integer>> selector, int bitIndex, List<Integer> nrs1) {
 		if (nrs1.size() == 1) {
 			return nrs1.get(0);
 		}
-		if (downwardCounter < 0) {
+		if (bitIndex < 0) {
 			throw new IllegalArgumentException("bitIndex <0 and still too many items "+ nrs1);
 		}
-		Map<Boolean, List<Integer>> mappedByBit = groupByBitIndex(nrs1, downwardCounter);
+		Map<Boolean, List<Integer>> mappedByBit = groupByBitIndex(nrs1, bitIndex);
 		List<Integer> remainingItems = selector.apply(mappedByBit);
-		return reduceListTillOne(selector, downwardCounter - 1, remainingItems);
+		return reduceListTillOne(selector, bitIndex - 1, remainingItems);
 	}
 
 	private Map<Boolean, List<Integer>> groupByBitIndex(List<Integer> nrs1, int downwardCounter) {

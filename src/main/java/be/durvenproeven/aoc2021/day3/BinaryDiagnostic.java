b03d86aab9e36bf6f389a8051a357fa93d3ac2d7
karlmis <karlmissiaen@gmail.com>
2021-12-03 08:07:36 +0100

    day 3 initial second part

--- src/main/java/be/durvenproeven/aoc2021/day3/BinaryDiagnostic.java
+++ src/main/java/be/durvenproeven/aoc2021/day3/BinaryDiagnostic.java
@@ -1,15 +1,20 @@
 package be.durvenproeven.aoc2021.day3;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class BinaryDiagnostic {
 	private final int size;
 	private int gammaRate;
+	private int ogygenGeneratorRating;
+	private final List<Integer> nrs;
 
 	public BinaryDiagnostic(List<String> binaries, int size) {
 		this.size = size;
-		List<Integer> nrs = binaries.stream()
+		nrs = binaries.stream()
 				.map(s -> Integer.parseInt(s, 2))
 				.toList();
 		for (int i = 0; i < size; i++) {
@@ -21,6 +26,48 @@ public class BinaryDiagnostic {
 				gammaRate+=  powerOf2(powerOf2);
 			}
 		}
+
+
+
+	}
+
+	private int getSize(List<Integer> integers) {
+		if (integers==null){
+			return 0;
+		}
+		return integers.size();
+	}
+
+	public int getOgyGeneratorRating(){
+		List<Integer> nrs1= new ArrayList<>(nrs);
+		int downwardCounter= size-1;
+		while(nrs1.size()>1) {
+			int bitNr= downwardCounter;
+			Map<Boolean, List<Integer>> collect = nrs1.stream().collect(Collectors.groupingBy(nr -> BigInteger.valueOf(nr).testBit(bitNr)));
+			if ( getSize(collect.get(false)) > getSize(collect.get(true))){
+				nrs1= collect.get(false);
+			} else {
+				nrs1= collect.get(true);
+			}
+			downwardCounter--;
+		}
+		return nrs1.get(0);
+	}
+
+	public int getCo2ScrubberRating(){
+		List<Integer> nrs1= new ArrayList<>(nrs);
+		int downwardCounter= size-1;
+		while(nrs1.size()>1) {
+			int bitNr= downwardCounter;
+			Map<Boolean, List<Integer>> collect = nrs1.stream().collect(Collectors.groupingBy(nr -> BigInteger.valueOf(nr).testBit(bitNr)));
+			if ( getSize(collect.get(true)) < getSize(collect.get(false))){
+				nrs1= collect.get(true);
+			} else {
+				nrs1= collect.get(false);
+			}
+			downwardCounter--;
+		}
+		return nrs1.get(0);
 	}
 
 	private int powerOf2(int powerOf2) {
--- src/test/java/be/durvenproeven/aoc2021/day3/BinaryDiagnosticTest.java
+++ src/test/java/be/durvenproeven/aoc2021/day3/BinaryDiagnosticTest.java
@@ -11,7 +11,7 @@ class BinaryDiagnosticTest {
 
 	@Test
 	void getGammaRate() {
-		assertThat(new BinaryDiagnostic(List.of("001","001"),3).getGammaRate()).isEqualTo(1);
+		assertThat(new BinaryDiagnostic(List.of("001","001","010"),3).getGammaRate()).isEqualTo(1);
 		assertThat(new BinaryDiagnostic(List.of("100","001","001"),3).getGammaRate()).isEqualTo(1);
 	}
 
@@ -43,4 +43,42 @@ class BinaryDiagnosticTest {
 		BinaryDiagnostic binaryDiagnostic = new BinaryDiagnostic(inputNrs, 12);
 		assertThat(binaryDiagnostic.getEpsilonRate()* binaryDiagnostic.getGammaRate()).isEqualTo(3923414);
 	}
+
+	@Test
+	void getOgyGeneratorRating() {
+		assertThat(new BinaryDiagnostic(List.of("100","001","011"),3).getOgyGeneratorRating()).isEqualTo(3);
+	}
+	@Test
+	void getCo2ScrubberRating() {
+		assertThat(new BinaryDiagnostic(List.of("100","001","011"),3).getCo2ScrubberRating()).isEqualTo(4);
+	}
+
+	@Test
+	void getSecondRates_SimpleGivenExample() {
+		BinaryDiagnostic binaryDiagnostic = new BinaryDiagnostic(List.of("00100",
+				"11110",
+				"10110",
+				"10111",
+				"10101",
+				"01111",
+				"00111",
+				"11100",
+				"10000",
+				"11001",
+				"00010",
+				"01010"), 5);
+		assertThat(binaryDiagnostic.getOgyGeneratorRating()).isEqualTo(23);
+		assertThat(binaryDiagnostic.getCo2ScrubberRating()).isEqualTo(10);
+		assertThat(binaryDiagnostic.getOgyGeneratorRating()* binaryDiagnostic.getCo2ScrubberRating()).isEqualTo(230);
+	}
+
+	@Test
+	void getRatesSecondary_RealExample() {
+		List<String> inputNrs = LineResolver.getStringStreamOfFile("day3.txt")
+				.toList();
+
+		BinaryDiagnostic binaryDiagnostic = new BinaryDiagnostic(inputNrs, 12);
+		assertThat(binaryDiagnostic.getOgyGeneratorRating()* binaryDiagnostic.getCo2ScrubberRating()).isEqualTo(5852595);
+	}
+
 }

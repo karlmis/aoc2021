513fe5b0de6e35ade992c07148e66fe673469f0f
karlmis <karlmissiaen@gmail.com>
2021-12-10 12:08:02 +0100

    day10: first commit

---
+++ src/main/java/be/durvenproeven/aoc2021/day10/SyntaxScoring.java
@@ -0,0 +1,179 @@
+package be.durvenproeven.aoc2021.day10;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class SyntaxScoring {
+	private final String line;
+
+	public SyntaxScoring(String line) {
+		this.line = line;
+	}
+
+	long getCompletionScore() {
+		return findMissing()
+				.map(ClosingCharacters::getCompletionScore)
+				.orElse(0L);
+
+	}
+
+	static long getCompletionScoreOf(List<String> l) {
+		long[] longs = l.stream()
+				.map(SyntaxScoring::new)
+				.mapToLong(SyntaxScoring::getCompletionScore)
+				.filter(i -> i != 0)
+				.sorted().toArray();
+		return longs[longs.length / 2];
+	}
+
+	public Optional<List<ClosingCharacters>> findMissing() {
+		if (findError().isPresent()) {
+			return Optional.empty();
+		}
+		List<String> strings = getStringListWithoutCouples();
+		Collections.reverse(strings);
+		return Optional.of(strings.stream()
+				.map(s -> ClosingCharacters.isOpening(s).orElseThrow(() -> new IllegalArgumentException("nothing found for " + s)))
+				.toList());
+	}
+
+	public Optional<ErrorResult> findError() {
+		List<String> strings = getStringListWithoutCouples();
+		for (int i = 1; i < strings.size(); i++) {
+			Optional<ClosingCharacters> closing = ClosingCharacters.isClosing(strings.get(i));
+			if (closing.isPresent()) {
+				return Optional.of(new ErrorResult(ClosingCharacters.isOpening(strings.get(i - 1)).get(), closing.get()));
+			}
+		}
+		return Optional.empty();
+	}
+
+	private List<String> getStringListWithoutCouples() {
+		List<String> strings = line.chars()
+				.mapToObj(Character::toString)
+				.collect(Collectors.toCollection(ArrayList::new));
+		int previousSize = strings.size();
+		removePairs(strings);
+		while (strings.size() < previousSize) {
+			previousSize = strings.size();
+			removePairs(strings);
+			System.out.println("after:" + strings);
+		}
+		return strings;
+	}
+
+	static int getScore(List<String> list) {
+		return list.stream()
+				.map(SyntaxScoring::new)
+				.map(SyntaxScoring::findError)
+				.flatMap(Optional::stream)
+				.mapToInt(ErrorResult::getScore)
+				.sum();
+	}
+
+	private void removePairs(List<String> strings) {
+		System.out.println("before:" + strings);
+		List<Integer> toRemove = new ArrayList<>();
+		for (int i = 1; i < strings.size(); i++) {
+			if (isClosing(strings.get(i - 1), strings.get(i))) {
+				toRemove.add(0, i - 1);
+				toRemove.add(0, i);
+			}
+		}
+		toRemove.forEach(index -> strings.remove(index.intValue()));
+	}
+
+	private boolean isClosing(String first, String second) {
+		return ClosingCharacters.isPair(first, second);
+	}
+
+	static class ErrorResult {
+		ClosingCharacters expected, found;
+
+		public ErrorResult(ClosingCharacters expected, ClosingCharacters found) {
+			this.expected = expected;
+			this.found = found;
+		}
+
+		public int getScore() {
+			return found.getErrorScore();
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) return true;
+			if (o == null || getClass() != o.getClass()) return false;
+			ErrorResult that = (ErrorResult) o;
+			return expected == that.expected && found == that.found;
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(expected, found);
+		}
+
+		@Override
+		public String toString() {
+			return "ErrorResult{" +
+					"expected=" + expected +
+					", found=" + found +
+					'}';
+		}
+	}
+
+	enum ClosingCharacters {
+		PARENTHESIS("(", ")", 3, 1),
+		BRACKET("[", "]", 57, 2),
+		BRACES("{", "}", 1197, 3),
+		SMALLER_GREATER("<", ">", 25137, 4);
+
+		private final String leftSign;
+		private final String rightSign;
+		private final int errorScore;
+		private final int completionScore;
+
+		ClosingCharacters(String leftSign, String rightSign, int errorScore, int completionScore) {
+			this.leftSign = leftSign;
+			this.rightSign = rightSign;
+			this.errorScore = errorScore;
+			this.completionScore = completionScore;
+		}
+
+		public int getErrorScore() {
+			return errorScore;
+		}
+
+		public int getCompletionScore() {
+			return completionScore;
+		}
+
+		public static boolean isPair(String first, String second) {
+			return Arrays.stream(values())
+					.anyMatch(v -> v.leftSign.equals(first) && v.rightSign.equals(second));
+		}
+
+		public static Optional<ClosingCharacters> isClosing(String first) {
+			return Arrays.stream(values())
+					.filter(v -> v.rightSign.equals(first))
+					.findFirst();
+		}
+
+		public static Optional<ClosingCharacters> isOpening(String first) {
+			return Arrays.stream(values())
+					.filter(v -> v.leftSign.equals(first))
+					.findFirst();
+		}
+
+		public static long getCompletionScore(List<ClosingCharacters> list) {
+			return list.stream()
+					.mapToLong(ClosingCharacters::getCompletionScore)
+					.reduce(0, (pr, nr) -> pr * 5 + nr);
+
+		}
+	}
+}
---
+++ src/test/java/be/durvenproeven/aoc2021/day10/SyntaxScoringTest.java
@@ -0,0 +1,119 @@
+package be.durvenproeven.aoc2021.day10;
+
+import be.durvenproeven.aoc2021.LineResolver;
+import org.assertj.core.api.Condition;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Optional;
+
+import static be.durvenproeven.aoc2021.day10.SyntaxScoring.ClosingCharacters.BRACES;
+import static be.durvenproeven.aoc2021.day10.SyntaxScoring.ClosingCharacters.BRACKET;
+import static be.durvenproeven.aoc2021.day10.SyntaxScoring.ClosingCharacters.PARENTHESIS;
+import static be.durvenproeven.aoc2021.day10.SyntaxScoring.ClosingCharacters.SMALLER_GREATER;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SyntaxScoringTest {
+
+
+	@Test
+	void findError() {
+		assertThat(new SyntaxScoring("(]").findError()).get()
+				.isEqualTo(new SyntaxScoring.ErrorResult(PARENTHESIS, BRACKET));
+		assertThat(new SyntaxScoring("{()()()>").findError()).get()
+				.isEqualTo(new SyntaxScoring.ErrorResult(BRACES, SMALLER_GREATER));
+		assertThat(new SyntaxScoring("(((()))}").findError()).get()
+				.isEqualTo(new SyntaxScoring.ErrorResult(PARENTHESIS, BRACES));
+		assertThat(new SyntaxScoring("<([]){()}[{}])").findError()).get()
+				.isEqualTo(new SyntaxScoring.ErrorResult(SMALLER_GREATER, PARENTHESIS));
+	}
+
+	@Test
+	void findError_GivenExample() {
+		assertThat(new SyntaxScoring("[({(<(())[]>[[{[]{<()<>>").findError()).isEmpty();
+		assertThat(new SyntaxScoring("[(()[<>])]({[<{<<[]>>(").findError()).isEmpty();
+		assertThat(new SyntaxScoring("{([(<{}[<>[]}>{[]{[(<()>").findError()).get().isEqualTo(new SyntaxScoring.ErrorResult(BRACKET, BRACES));
+		assertThat(new SyntaxScoring("(((({<>}<{<{<>}{[]{[]{}").findError()).isEmpty();
+		assertThat(new SyntaxScoring("[[<[([]))<([[{}[[()]]]").findError()).get().isEqualTo(new SyntaxScoring.ErrorResult(BRACKET, PARENTHESIS));
+		assertThat(new SyntaxScoring("[{[{({}]{}}([{[{{{}}([]").findError()).get().isEqualTo(new SyntaxScoring.ErrorResult(PARENTHESIS, BRACKET));
+		assertThat(new SyntaxScoring("{<[[]]>}<{[{[{[]{()[[[]").findError()).isEmpty();
+		assertThat(new SyntaxScoring("[<(<(<(<{}))><([]([]()").findError()).get().isEqualTo(new SyntaxScoring.ErrorResult(SMALLER_GREATER, PARENTHESIS));
+		assertThat(new SyntaxScoring("<{([([[(<>()){}]>(<<{{").findError()).get().isEqualTo(new SyntaxScoring.ErrorResult(BRACKET, SMALLER_GREATER));
+		assertThat(new SyntaxScoring("<{([{{}}[<[[[<>{}]]]>[]]").findError()).isEmpty();
+	}
+
+	@Test
+	void getScore_GivenExample() {
+		assertThat(SyntaxScoring.getScore(List.of(
+				"[({(<(())[]>[[{[]{<()<>>",
+				"[(()[<>])]({[<{<<[]>>(",
+				"{([(<{}[<>[]}>{[]{[(<()>",
+				"(((({<>}<{<{<>}{[]{[]{}",
+				"[[<[([]))<([[{}[[()]]]",
+				"[{[{({}]{}}([{[{{{}}([]",
+				"{<[[]]>}<{[{[{[]{()[[[]",
+				"[<(<(<(<{}))><([]([]()",
+				"<{([([[(<>()){}]>(<<{{",
+				"<{([{{}}[<[[[<>{}]]]>[]]"))
+		).isEqualTo(26397);
+	}
+
+	@Test
+	void getScore_RealExample() {
+		List<String> strings = LineResolver.getStringStreamOfFile("day10syntax.txt").toList();
+
+		assertThat(SyntaxScoring.getScore(strings)).isEqualTo(323613);
+	}
+
+	@Test
+	void findMissing() {
+		assertThat(new SyntaxScoring("[({(<(())[]>[[{[]{<()<>>").findMissing()).has(closing("}}]])})]"));
+		assertThat(new SyntaxScoring("[(()[<>])]({[<{<<[]>>(").findMissing()).has(closing(")}>]})"));
+		assertThat(new SyntaxScoring("(((({<>}<{<{<>}{[]{[]{}").findMissing()).has(closing("}}>}>))))"));
+		assertThat(new SyntaxScoring("{<[[]]>}<{[{[{[]{()[[[]").findMissing()).has(closing("]]}}]}]}>"));
+		assertThat(new SyntaxScoring("<{([{{}}[<[[[<>{}]]]>[]]").findMissing()).has(closing("])}>"));
+	}
+
+	@Test
+	void getCompletionScore() {
+		assertThat(new SyntaxScoring("[({(<(())[]>[[{[]{<()<>>").getCompletionScore()).isEqualTo(288957);
+		assertThat(new SyntaxScoring("[(()[<>])]({[<{<<[]>>(").getCompletionScore()).isEqualTo(5566);
+		assertThat(new SyntaxScoring("(((({<>}<{<{<>}{[]{[]{}").getCompletionScore()).isEqualTo(1480781);
+		assertThat(new SyntaxScoring("{<[[]]>}<{[{[{[]{()[[[]").getCompletionScore()).isEqualTo(995444);
+		assertThat(new SyntaxScoring("<{([{{}}[<[[[<>{}]]]>[]]").getCompletionScore()).isEqualTo(294);
+
+	}
+
+	@Test
+	void getCompletionScoreOf_GivenExample() {
+		assertThat(SyntaxScoring.getCompletionScoreOf(List.of(
+				"[({(<(())[]>[[{[]{<()<>>",
+				"[(()[<>])]({[<{<<[]>>(",
+				"{([(<{}[<>[]}>{[]{[(<()>",
+				"(((({<>}<{<{<>}{[]{[]{}",
+				"[[<[([]))<([[{}[[()]]]",
+				"[{[{({}]{}}([{[{{{}}([]",
+				"{<[[]]>}<{[{[{[]{()[[[]",
+				"[<(<(<(<{}))><([]([]()",
+				"<{([([[(<>()){}]>(<<{{",
+				"<{([{{}}[<[[[<>{}]]]>[]]"))
+		).isEqualTo(288957);
+	}
+
+	@Test
+	void getCompletionScoreOf_RealExample() {
+		List<String> strings = LineResolver.getStringStreamOfFile("day10syntax.txt").toList();
+
+		assertThat(SyntaxScoring.getCompletionScoreOf(strings)).isEqualTo(3103006161L);
+
+	}
+
+		private Condition<? super Optional<List<SyntaxScoring.ClosingCharacters>>> closing(String s) {
+		List<SyntaxScoring.ClosingCharacters> closing = s.chars()
+				.mapToObj(Character::toString)
+				.map(SyntaxScoring.ClosingCharacters::isClosing)
+				.map(Optional::orElseThrow)
+				.toList();
+		return new Condition<>(t -> t.isPresent() && t.get().equals(closing), "%s: %s", s, closing);
+	}
+}

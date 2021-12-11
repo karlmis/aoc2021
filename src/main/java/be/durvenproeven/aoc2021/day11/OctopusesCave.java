078132e5ef76cc15fbdbd9511cfa4bb30e40031f
karlmis <karlmissiaen@gmail.com>
2021-12-11 11:16:13 +0100

    day 11: initial checkin

---
+++ src/main/java/be/durvenproeven/aoc2021/day11/OctopusesCave.java
@@ -0,0 +1,131 @@
+package be.durvenproeven.aoc2021.day11;
+
+import be.durvenproeven.aoc2021.NumberUtils;
+import be.durvenproeven.aoc2021.day9.Coordinates;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+
+public class OctopusesCave {
+	private static final int MAX_HEIGHT = 9;
+	private final int[][] nrs;
+	private final Coordinates maxCoordinate;
+
+
+	public OctopusesCave(List<String> input) {
+		nrs = new int[input.size()][];
+		for (int i = 0; i < input.size(); i++) {
+			nrs[i] = NumberUtils.toIntArray(input.get(i));
+		}
+		maxCoordinate = new Coordinates(input.size(), input.get(0).length());
+	}
+
+	public boolean allFlash() {
+		for (int[] nr : nrs) {
+			for (int i : nr) {
+				if (i!= 0){
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	int nextStep() {
+		Coordinates.getAllCoordinates(maxCoordinate).stream()
+				.forEach(co -> setValue(co, getValue(co)+1));
+		List<Coordinates> lightningCoordinates = new ArrayList<>();
+		List<Coordinates> allChanged = new ArrayList<>();
+		Coordinates.getAllCoordinates(maxCoordinate).stream()
+				.filter(co -> getValue(co) > 9)
+				.forEach(lightningCoordinates::add);
+		allChanged.addAll(lightningCoordinates);
+		while (!lightningCoordinates.isEmpty()) {
+			Map<Coordinates, Long> neighbours = lightningCoordinates.stream()
+					.flatMap(co -> getNeighbours(co).stream())
+					.filter(o -> !allChanged.contains(o))
+					.filter(co -> getValue(co)<=9)
+					.collect(groupingBy(Function.identity(), counting()));
+
+			lightningCoordinates.clear();
+			for (Map.Entry<Coordinates, Long> entry : neighbours.entrySet()) {
+				int newValue = getValue(entry.getKey()) + entry.getValue().intValue();
+				setValue(entry.getKey(), newValue);
+				if (newValue > 9 && !allChanged.contains(entry.getKey())) {
+					lightningCoordinates.add(entry.getKey());
+				}
+			}
+			allChanged.addAll(lightningCoordinates);
+		}
+		Coordinates.getAllCoordinates(maxCoordinate).stream()
+				.filter(co -> getValue(co) > 9)
+				.forEach(co -> setValue(co, 0));
+		return allChanged.size();
+	}
+
+	private List<Coordinates> getNeighbours(Coordinates co) {
+		return Stream.of(
+				getCoordinate(co.getX() + 1, co.getY() - 1),
+				getCoordinate(co.getX() + 1, co.getY()),
+				getCoordinate(co.getX() + 1, co.getY() + 1),
+				getCoordinate(co.getX(), co.getY() + 1),
+				getCoordinate(co.getX() - 1, co.getY() - 1),
+				getCoordinate(co.getX() - 1, co.getY()),
+				getCoordinate(co.getX() - 1, co.getY() + 1),
+				getCoordinate(co.getX(), co.getY() - 1))
+				.flatMap(Optional::stream)
+				.toList();
+	}
+
+	private Optional<Coordinates> getCoordinate(int x, int y) {
+		Coordinates coordinates = new Coordinates(x, y);
+		if (isValid(coordinates)) {
+			return Optional.of(coordinates);
+		}
+		return Optional.empty();
+	}
+
+	private boolean isValid(Coordinates coordinates1) {
+		return coordinates1.isInFirstQuadrant() && coordinates1.isSmallerThen(maxCoordinate);
+	}
+
+	private int getValue(Coordinates co) {
+		return nrs[co.getX()][co.getY()];
+	}
+
+	private int setValue(Coordinates co, int i) {
+		return nrs[co.getX()][co.getY()] = i;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		OctopusesCave that = (OctopusesCave) o;
+		return Arrays.deepEquals(nrs, that.nrs) && Objects.equals(maxCoordinate, that.maxCoordinate);
+	}
+
+	@Override
+	public int hashCode() {
+		int result = Objects.hash(maxCoordinate);
+		result = 31 * result + Arrays.hashCode(nrs);
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		return "OctopusesCave{" +
+				"nrs=" + Arrays.deepToString(nrs) +
+				'}';
+	}
+}
---
+++ src/test/java/be/durvenproeven/aoc2021/day11/OctopusesCaveTest.java
@@ -0,0 +1,260 @@
+package be.durvenproeven.aoc2021.day11;
+
+import be.durvenproeven.aoc2021.LineResolver;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class OctopusesCaveTest {
+
+	@Test
+	void nextStep_SimpleExample() {
+		OctopusesCave octopusesCave = new OctopusesCave(List.of("11111",
+				"19991",
+				"19191",
+				"19991",
+				"11111"));
+
+		assertThat(octopusesCave.nextStep()).isEqualTo(9);
+		assertThat(octopusesCave).isEqualTo(new OctopusesCave(List.of(
+				"34543",
+				"40004",
+				"50005",
+				"40004",
+				"34543")));
+
+		assertThat(octopusesCave.nextStep()).isEqualTo(0);
+		assertThat(octopusesCave).isEqualTo(new OctopusesCave(List.of(
+				"45654",
+				"51115",
+				"61116",
+				"51115",
+				"45654")));
+	}
+
+	@Test
+	void nextStep_GivenExample() {
+		OctopusesCave octopusesCave = new OctopusesCave(List.of(
+				"5483143223",
+				"2745854711",
+				"5264556173",
+				"6141336146",
+				"6357385478",
+				"4167524645",
+				"2176841721",
+				"6882881134",
+				"4846848554",
+				"5283751526"));
+		int nr = 0;//1
+		nr += octopusesCave.nextStep();
+		assertThat(octopusesCave).isEqualTo(new OctopusesCave(List.of(
+				"6594254334",
+				"3856965822",
+				"6375667284",
+				"7252447257",
+				"7468496589",
+				"5278635756",
+				"3287952832",
+				"7993992245",
+				"5957959665",
+				"6394862637")
+		));
+//2
+		nr += octopusesCave.nextStep();
+		assertThat(octopusesCave).isEqualTo(new OctopusesCave(List.of(
+				"8807476555",
+				"5089087054",
+				"8597889608",
+				"8485769600",
+				"8700908800",
+				"6600088989",
+				"6800005943",
+				"0000007456",
+				"9000000876",
+				"8700006848")
+		));
+//3
+		nr += octopusesCave.nextStep();
+		assertThat(octopusesCave).isEqualTo(new OctopusesCave(List.of(
+				"0050900866",
+				"8500800575",
+				"9900000039",
+				"9700000041",
+				"9935080063",
+				"7712300000",
+				"7911250009",
+				"2211130000",
+				"0421125000",
+				"0021119000")
+		));
+//4
+		nr += octopusesCave.nextStep();
+		assertThat(octopusesCave).isEqualTo(new OctopusesCave(List.of(
+				"2263031977",
+				"0923031697",
+				"0032221150",
+				"0041111163",
+				"0076191174",
+				"0053411122",
+				"0042361120",
+				"5532241122",
+				"1532247211",
+				"1132230211")
+		));
+//5
+		nr += octopusesCave.nextStep();
+		assertThat(octopusesCave).isEqualTo(new OctopusesCave(List.of(
+				"4484144000",
+				"2044144000",
+				"2253333493",
+				"1152333274",
+				"1187303285",
+				"1164633233",
+				"1153472231",
+				"6643352233",
+				"2643358322",
+				"2243341322")
+		));
+//6
+		nr += octopusesCave.nextStep();
+		assertThat(octopusesCave).isEqualTo(new OctopusesCave(List.of(
+				"5595255111",
+				"3155255222",
+				"3364444605",
+				"2263444496",
+				"2298414396",
+				"2275744344",
+				"2264583342",
+				"7754463344",
+				"3754469433",
+				"3354452433")
+		));
+//7
+		nr += octopusesCave.nextStep();
+		assertThat(octopusesCave).isEqualTo(new OctopusesCave(List.of(
+				"6707366222",
+				"4377366333",
+				"4475555827",
+				"3496655709",
+				"3500625609",
+				"3509955566",
+				"3486694453",
+				"8865585555",
+				"4865580644",
+				"4465574644")
+		));
+//8
+		nr += octopusesCave.nextStep();
+		assertThat(octopusesCave).isEqualTo(new OctopusesCave(List.of(
+				"7818477333",
+				"5488477444",
+				"5697666949",
+				"4608766830",
+				"4734946730",
+				"4740097688",
+				"6900007564",
+				"0000009666",
+				"8000004755",
+				"6800007755")
+		));
+//9
+		nr += octopusesCave.nextStep();
+		assertThat(octopusesCave).isEqualTo(new OctopusesCave(List.of(
+				"9060000644",
+				"7800000976",
+				"6900000080",
+				"5840000082",
+				"5858000093",
+				"6962400000",
+				"8021250009",
+				"2221130009",
+				"9111128097",
+				"7911119976")
+		));
+
+		nr += octopusesCave.nextStep();
+		assertThat(octopusesCave).isEqualTo(new OctopusesCave(List.of(
+				"0481112976",
+				"0031112009",
+				"0041112504",
+				"0081111406",
+				"0099111306",
+				"0093511233",
+				"0442361130",
+				"5532252350",
+				"0532250600",
+				"0032240000")
+		));
+
+//		int sum = IntStream.range(0, 10)
+//				.map(i -> octopusesCave.nextStep())
+//				.sum();
+		assertThat(nr).isEqualTo(204);
+	}
+
+	@Test
+	void nextStep_GivenExampleCount() {
+		OctopusesCave octopusesCave = new OctopusesCave(List.of(
+				"5483143223",
+				"2745854711",
+				"5264556173",
+				"6141336146",
+				"6357385478",
+				"4167524645",
+				"2176841721",
+				"6882881134",
+				"4846848554",
+				"5283751526"));
+		int sum = IntStream.range(0, 100)
+				.map(i -> octopusesCave.nextStep())
+				.sum();
+		assertThat(sum).isEqualTo(1656);
+	}
+
+	@Test
+	void nextStep_RealInput() {
+		List<String> strings = LineResolver.getStringStreamOfFile("day11lumniscentoctopuses.txt").toList();
+		OctopusesCave octopusesCave = new OctopusesCave(strings);
+		int sum = IntStream.range(0, 100)
+				.map(i -> octopusesCave.nextStep())
+				.sum();
+		assertThat(sum).isEqualTo(1681);
+	}
+
+	@Test
+	void allFlashes_GivenExample() {
+		OctopusesCave octopusesCave = new OctopusesCave(List.of(
+				"5483143223",
+				"2745854711",
+				"5264556173",
+				"6141336146",
+				"6357385478",
+				"4167524645",
+				"2176841721",
+				"6882881134",
+				"4846848554",
+				"5283751526"));
+		assertThat(getStepWhenAllFlashes(octopusesCave)).isEqualTo(195);
+	}
+
+	@Test
+	void allFlashes_RealExample() {
+		List<String> strings = LineResolver.getStringStreamOfFile("day11lumniscentoctopuses.txt").toList();
+		OctopusesCave octopusesCave = new OctopusesCave(strings);
+
+		assertThat(getStepWhenAllFlashes(octopusesCave)).isEqualTo(276);
+	}
+
+	private int getStepWhenAllFlashes(OctopusesCave octopusesCave) {
+		int counter = 0;
+		while (!octopusesCave.allFlash()) {
+			counter++;
+			octopusesCave.nextStep();
+		}
+		return counter;
+	}
+
+}

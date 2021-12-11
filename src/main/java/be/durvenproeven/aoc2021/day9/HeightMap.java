97fed137174a5cd275237a22515133f5d71540e0
karlmis <karlmissiaen@gmail.com>
2021-12-11 13:46:03 +0100

    imports

--- src/main/java/be/durvenproeven/aoc2021/day9/HeightMap.java
+++ src/main/java/be/durvenproeven/aoc2021/day9/HeightMap.java
@@ -3,10 +3,8 @@ package be.durvenproeven.aoc2021.day9;
 
 import be.durvenproeven.aoc2021.CollectionUtils;
 import be.durvenproeven.aoc2021.Coordinates;
-import be.durvenproeven.aoc2021.Direction;
 import be.durvenproeven.aoc2021.NumberUtils;
 
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
